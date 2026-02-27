efdf031de64a7206f9173f5e3f7ce21a9fccfd0d
ShahrairRahat <shahriarrahat@outlook.com>
2026-02-27 15:32:51 +0600

    migration to ern

--- vite-react/extract_keys.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const cache = JSON.parse(fs.readFileSync('translations_cache.json', 'utf8'));
-const ben = cache['ben'];
-
-// Dump just the english keys
-const keys = Object.keys(ben);
-fs.writeFileSync('ben_keys.txt', keys.join('\n'));
-console.log('Saved to ben_keys.txt');
