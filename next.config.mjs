eac76327659802245155121a00418cbc91154598
Hridhyapaul <Hpdesign2021@gmail.com>
2026-04-07 18:04:44 +0600

    update: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   // Allow loading `/_next/*` dev assets when the app is opened via LAN IP (not localhost).
   // See: https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins
-  allowedDevOrigins: ["http://192.168.7.39"],
+  compress: false,
+  output: 'standalone',
+  trailingSlash: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
